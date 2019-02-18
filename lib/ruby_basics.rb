af22c319c0028bb95656fd40e07ae568f461b7f9
modern-intelligence-1932 <francisco.torresrojo@yale.edu>
2019-02-18 14:18:51 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,18 +1,19 @@
 def division( num1, num2 )
-num1=42
-num2=7
+num1 = 42
+num2 = 7
 end
 
 def assign_variable(value)
-  value= "Bob"
+  value = "Bob"
 end
 
-def argue
-  phrase= "Im right and youre wrong!"
+def argue(phrase)
+  phrase = "Im right and youre wrong!"
 end
 
-def greeting
-
+def greeting (greeting, name)
+  greeting = "Hi there, "
+name = "Bobby!"
 end
 
 def return_a_value
