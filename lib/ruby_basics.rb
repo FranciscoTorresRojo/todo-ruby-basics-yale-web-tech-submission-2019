3803cc95153da2bb92e281bb4afbd30a301dd159
modern-intelligence-1932 <francisco.torresrojo@yale.edu>
2019-02-18 14:52:00 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -28,8 +28,7 @@ def last_evaluated_value (last_evaluated_value)
 end
 
 
-def pizza_party(pizza_party, cheese)
-pizza_party="cheese"
-pizza_party="pepperoni"
+def pizza_party(cheese,pepperoni)
+pizza_party ="cheese"
+pizza_party ="pepperoni"
 end
-end 
